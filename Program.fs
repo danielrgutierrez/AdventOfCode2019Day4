e1fd999f9a5e70dbef59d4b15ac2df9ad3ac1401
Daniel Gutierrez <daniel.r.gutierrez.94@gmail.com>
2020-04-14 13:59:51 -0700

    Implement Part 2 solution

--- Program.fs
+++ Program.fs
@@ -25,6 +25,24 @@ let listHasAdjacentDuplicates (list: int list) =
     |> listHasAdjacentDuplicatesAux
     |> List.contains true
 
+/// <summary>Checks if a list has at least one pair of adjacent duplicates.</summary>
+/// <param name="list">The list to check</param>
+/// <returns><c>true</c> if the list has at least one pair of adjacent duplicates.</returns>
+let listHasAtLeastOneAdjacentDuplicatePair (list: int list) =
+    let insert element state =
+        match state with
+        | current :: rest ->
+            match current with
+            | head :: _ when element = head -> (element :: current) :: rest
+            | _ -> [ element ] :: state
+        | _ -> [ [ element ] ]
+
+    let listOfAdjacentIntLists = List.foldBack insert list []
+
+    listOfAdjacentIntLists
+    |> List.map (fun list -> list.Length)
+    |> List.exists (fun length -> length = 2)
+
 /// <summary>Checks a list of ints to see if the list never decreases from left to right.</summary>
 /// <param name="list">A list of ints.</param>
 /// <returns><c>true</c> if the list never decreases from left to right.</returns>
@@ -40,19 +58,37 @@ let listNeverDecreases (list: int list) =
     |> List.contains false
     |> not
 
-/// <summary>Checks if a password is valid. A password is valid if it has at least two adjacent
-/// duplicate digits and never decreases from left to right.</summary>
+/// <summary>Checks if a password is valid for Part 1. A password is valid if it has at least
+/// two adjacent duplicate digits and never decreases from left to right.</summary>
 /// <param name="password">The password to check.</param>
 /// <returns> <c>true</c> if the password is valid.</returns>
-let isValidPassword (password: int) =
+let isValidPasswordPart1 (password: int) =
     let list = intToIntList password
     listHasAdjacentDuplicates list && listNeverDecreases list
 
+/// <summary>Checks if a password is valid for Part 2. A password is valid if at has at least one
+/// pair of duplicate digits (and larger groups of duplicate digits do not count) and never
+/// decreases from left to right.</summary>
+/// <param name="password"></param>
+/// <returns><c>true</c> if the password is valid.</returns>
+let isValidPasswordPart2 (password: int) =
+    let list = intToIntList password
+    listHasAtLeastOneAdjacentDuplicatePair list && listNeverDecreases list
+
 [<EntryPoint>]
 let main argv =
-    [ 123257 .. 647015 ]
-    |> List.map (isValidPassword)
-    |> List.filter (fun (b: bool) -> b <> false)
-    |> List.length
-    |> printfn "Part 1: The number of valid passwords is %d."
+    let part1Result =
+        [ 123257 .. 647015 ]
+        |> List.map (isValidPasswordPart1)
+        |> List.filter (fun (b: bool) -> b)
+        |> List.length
+
+    let part2Result =
+        [ 123257 .. 647015 ]
+        |> List.map (isValidPasswordPart2)
+        |> List.filter (fun (b: bool) -> b)
+        |> List.length
+
+    printfn "Part 1: The number of valid passwords is %d." part1Result
+    printfn "Part 2: The number of valid passwords is %d." part2Result
     0 // return an integer exit code
